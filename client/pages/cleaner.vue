289653c8b85cef860cb275678fe7417ed8073777
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-16 15:28:31 +0900

    時刻修正

--- client/pages/cleaner.vue
+++ client/pages/cleaner.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <div>ここ、年、曜日いらんな</div>
         <div class="time_add_cotainer">
-            <div>開始時刻</div>
-            <button @click="getStartTime">清掃を開始</button>
-            <div>{{ viewStartTime }}</div>
-            <div>終了時刻@{{ viewFinishedTime }}</div>
-            <button @click="getFinishTime">清掃を終了</button>
+            <div>開始時刻{{ viewStartTime }}</div>
+            <button onclick="this.disabled = true" @click="getStartTime">
+                清掃を開始
+            </button>
+            <div>終了時刻{{ viewFinishedTime }}</div>
+            <button onclick="this.disabled = true" @click="getFinishTime">
+                清掃を終了
+            </button>
         </div>
         <div class="room_add_container">
             <div>清掃部屋</div>
@@ -48,7 +50,6 @@ export default class AddRecord extends Vue {
 
     public getFinishTime() {
         this.finishedTime = new Date().getTime()
-        this.finishedTime = new Date().getTime()
         const date = new Date(this.finishedTime)
         const HH = `0${date.getHours()}`.slice(-2)
         const mm = `0${date.getMinutes()}`.slice(-2)
@@ -56,13 +57,15 @@ export default class AddRecord extends Vue {
         this.viewFinishedTime = `${HH}:${mm}:${ss}`
     }
 
-    // 投稿できた時にメッセージ出したい
     public async register() {
         this.blancRecord = await userInteractor.createNewRecord()
         this.blancRecord.startAt = this.startTime
         this.blancRecord.finishedAt = this.finishedTime
         this.blancRecord.room = this.roomValue
         await this.blancRecord.register()
+        window.alert(
+            '清掃記録は正常に送信されました。続けて別の清掃を記録するにはページをリロードしてください。'
+        )
     }
 }
 </script>
