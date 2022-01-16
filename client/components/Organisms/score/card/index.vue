825f31871e4a6aff094de5a98c22b57d97b17bc7
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-16 15:29:03 +0900

    スコアコンポーネント作った

---
+++ client/components/Organisms/score/card/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>清掃スコア{{ score }}</div>
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Prop } from 'nuxt-property-decorator'
+import { ScoreModel } from 'stage3-abr'
+export default class ScoreCard extends Vue {
+    @Prop({ required: true }) scoreModel!: ScoreModel
+
+    get score() {
+        return this.scoreModel.score
+    }
+}
+</script>
