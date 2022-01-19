4f98dbaf737b915f63845abded719e2c3bdc0b23
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-19 14:32:54 +0900

    いるページ作った

---
+++ client/pages/manager/manager_top.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            managerのためのトップページです。ここにアサイン済みの清掃を一覧で表示したいです
+        </div>
+        <div class="this_will_be_footer">
+            <nuxt-link
+                :to="{
+                    name: 'manager-unscored_record_list',
+                }"
+            >
+                <button>評価を開始する</button>
+            </nuxt-link>
+            <nuxt-link
+                :to="{
+                    name: 'manager-record_list',
+                }"
+            >
+                <button>評価を開始する</button>
+            </nuxt-link>
+        </div>
+    </div>
+</template>
