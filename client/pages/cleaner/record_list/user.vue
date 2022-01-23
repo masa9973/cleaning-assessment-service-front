8c82ee67bf51b11ea711d61ee779a0d6db11232f
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-23 18:01:34 +0900

    ユーザーの清掃記録表示

---
+++ client/pages/cleaner/record_list/user.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="record_list_associated_with_user">
+        <div>ここにユーザーのレコード一覧を表示する</div>
+        <div v-for="record in records"
+        :key="record.recordID">
+         <div>{{ record.recordID }}</div>
+        </div>
+        <link-button :to="{ name: 'cleaner-record_list-room' }">部屋に紐づくレコードを表示する</link-button>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { RecordModel, UserModel } from 'stage3-abr'
+import { userInteractor } from '~/api'
+import LinkButton from '@/components/Atom/LinkButton.vue'
+
+@Component({
+    components: {
+        LinkButton
+    }
+})
+
+export default class UserRecordList extends Vue {
+    public user: UserModel | null = null
+    public records: RecordModel[] = []
+    async created() {
+        this.user = await userInteractor.fetchMyUserModel()
+        this.records = await userInteractor.fetchRecordsByCleanerID(this.user.userID)
+    }
+}
+</script>
