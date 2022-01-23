0919ed7a8b21e6b37fea5e874e46075b72b31b8c
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-23 18:02:17 +0900

    cleanerLayou作成

---
+++ client/layouts/cleaner.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="cleaner_app_container">
+        <div class="app">
+            <nuxt />
+        </div>
+        <!-- footer -->
+        <div class="app_cleaner_footer">
+            <app-cleaner-footer />
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import AppCleanerFooter from '@/components/Organisms/common/app_footer/cleaner_footer.vue'
+@Component({
+    components: {
+        AppCleanerFooter,
+    },
+})
+export default class CleanerLayout extends Vue {
+}
+</script>
+<style lang="stylus" scoped>
+.app_cleaner_footer {
+    position: sticky;
+    bottom: 0;
+}
+</style>
