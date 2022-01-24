2e04f309b746a53663b9c90bd0b364ce8e640139
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-24 11:27:57 +0900

    レコードを表示

--- client/pages/cleaner/record_list/user.vue
+++ client/pages/cleaner/record_list/user.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="record_list_associated_with_user">
         <div>ここにユーザーのレコード一覧を表示する</div>
-        <div v-for="record in records"
-        :key="record.recordID">
-         <div>{{ record.recordID }}</div>
+        <div v-for="record in records" :key="record.recordID">
+            <record-card :record-model="record" />
         </div>
-        <link-button :to="{ name: 'cleaner-record_list-room' }">部屋に紐づくレコードを表示する</link-button>
+        <link-button :to="{ name: 'cleaner-record_list-room' }"
+            >部屋に紐づくレコードを表示する</link-button
+        >
     </div>
 </template>
 <script lang="ts">
@@ -13,19 +14,23 @@ import { Component, Vue } from 'nuxt-property-decorator'
 import { RecordModel, UserModel } from 'stage3-abr'
 import { userInteractor } from '~/api'
 import LinkButton from '@/components/Atom/LinkButton.vue'
+import RecordCard from '@/components/Organisms/record/card/index.vue'
 
 @Component({
+    layout: 'cleaner',
     components: {
-        LinkButton
-    }
+        LinkButton,
+        RecordCard,
+    },
 })
-
 export default class UserRecordList extends Vue {
     public user: UserModel | null = null
     public records: RecordModel[] = []
     async created() {
         this.user = await userInteractor.fetchMyUserModel()
-        this.records = await userInteractor.fetchRecordsByCleanerID(this.user.userID)
+        this.records = await userInteractor.fetchRecordsByCleanerID(
+            this.user.userID
+        )
     }
 }
 </script>
