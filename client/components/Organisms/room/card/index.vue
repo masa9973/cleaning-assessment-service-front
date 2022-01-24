d36ee5e00c6165190fdf09b65d62f8b459982fbf
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-24 11:33:53 +0900

    roomcard作成

---
+++ client/components/Organisms/room/card/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="room_card">
+        <div>
+            <div>{{ cleaningRoomName }}</div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import { RoomModel } from 'stage3-abr'
+import AppButton from '@/components/Atom/AppButton.vue'
+import { userInteractor } from '~/api'
+
+@Component({
+    components: {
+    AppButton,
+    },
+})
+export default class RoomCard extends Vue {
+    @Prop({ required: true }) roomID!: string
+    public roomModel: RoomModel | null = null
+    public cleaningRoomName: string = ''
+
+    async created() {
+        this.roomModel = await userInteractor.fetchRoomByRoomID(this.roomID)
+        this.cleaningRoomName = this.roomModel!.roomName
+    }
+}
+</script>
+
