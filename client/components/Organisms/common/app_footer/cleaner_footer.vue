2098cd6de44cb68b0cb308decdf9ce9014f6cad9
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-01-24 11:28:12 +0900

    フッター作成

---
+++ client/components/Organisms/common/app_footer/cleaner_footer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="footer_item_container">
+        <nuxt-link
+            :to="{
+                name: 'cleaner-cleaner_top',
+            }"
+        >
+            <div class="home_button">
+                <img src="~/assets/img/account_circle.png" />
+            </div>
+        </nuxt-link>
+        <nuxt-link
+            :to="{
+                name: 'cleaner-record_list-user',
+            }"
+        >
+            <div class="record_button">
+                <img src="~/assets/img/schedule.png" />
+            </div>
+        </nuxt-link>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class AppCleanerFooter extends Vue {}
+</script>
+<style lang="stylus" scoped>
+.footer_item_container {
+    display: flex;
+    // justify-content: space-between;
+    // align-items: center;
+    background-color: #c4c4c4;
+    .home_button {
+        display: flex;
+        // justify-content: center;
+        // align-items: center;
+    }
+    .record_button {
+        display: flex;
+        // justify-content: center;
+        // align-items: center;
+    }
+}
+</style>
---
+++ client/components/Organisms/common/app_footer/manager_footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="this_will_be_footer">
+            <nuxt-link
+                :to="{
+                    name: 'manager-unscored_record_list',
+                }"
+            >
+                <button>清掃記録の評価を開始する</button>
+            </nuxt-link>
+            <nuxt-link
+                :to="{
+                    name: 'manager-record_list',
+                }"
+            >
+                <button>過去の記録を見る</button>
+            </nuxt-link>
+            <nuxt-link
+                :to="{
+                    name: 'manager-config',
+                }"
+            >
+                <button>清掃者を追加する</button>
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class AppManagerFooter extends Vue {
+
+}
+</script>
